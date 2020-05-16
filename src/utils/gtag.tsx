5ce8a7f0e120d30aff1d0596bc93ba2839564315
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-16 14:43:37 -0300

    gtag workaround

--- src/utils/gtag.tsx
+++ src/utils/gtag.tsx
@@ -2,14 +2,14 @@ const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
-  window.gtag('config', GA_TRACKING_ID, {
+  (window as any).gtag('config', GA_TRACKING_ID, {
     page_path: url,
   })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
+  (window as any).gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
