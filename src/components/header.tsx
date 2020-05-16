62f2f427b964814877cf68a77abe72b6a57481e0
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-15 22:15:03 -0300

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,29 @@
 import useDarkMode from 'use-dark-mode'
+import { useRouter } from 'next/router'
+
+const HeaderLink = ({ children, ...props }) => {
+  const router = useRouter()
+
+  return router.route === props.href ? children : (
+    <a {...props}>
+      {children}
+    </a>
+  )
+}
+
 
 export function Header () {
-  const darkMode = useDarkMode(null, { storageKey: 'theme' })
-
   return (
     <div>
-        Title
-        <button onClick={darkMode.toggle}>toggle</button>
+      <HeaderLink href='/'>home</HeaderLink>
+      <HeaderLink href='/blog'>blog</HeaderLink>
+      <ToggleThemeButton />
     </div>
   )
+}
+
+const ToggleThemeButton = () => {
+  const darkMode = useDarkMode(null, { storageKey: 'theme' })
+
+  return <button onClick={darkMode.toggle}>toggle</button> 
 }
