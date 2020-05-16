b612f5b30fdf567e03567c098d66d3be9429e3c4
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-16 11:51:24 -0300

    fast refresh

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  webpack: function(config) {
+  webpack: function(config, { dev }) {
+    if (dev) {
+      config.devtool = 'cheap-module-source-map';
+    }
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
