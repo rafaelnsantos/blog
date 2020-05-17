4669ce264558e25d0cdd1abd32c1b300da02c22f
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-17 14:53:02 -0300

    preconnect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,8 @@ export default class MyDocument extends Document {
           <FallbackStyles />
           <Gtag />
           <PageView />
+          <link rel="preconnect" href="https://www.google-analytics.com" />
+          <link rel="dns-prefetch" href="https://www.google-analytics.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap"
             rel="stylesheet"
