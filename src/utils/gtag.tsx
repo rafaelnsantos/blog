3eac178fc4c144a9233ecc7c108b5add102bc2b3
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-17 16:37:47 -0300

    style

--- src/utils/gtag.tsx
+++ src/utils/gtag.tsx
@@ -1,11 +1,12 @@
-const GA_TRACKING_ID = process.env.GA_TRACKING_ID
+/* eslint-disable @typescript-eslint/camelcase */
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = url => {
+export const pageview = (url) => {
   (window as any).gtag('config', GA_TRACKING_ID, {
     page_path: url,
-  })
-}
+  });
+};
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
@@ -13,12 +14,15 @@ export const event = ({ action, category, label, value }) => {
     event_category: category,
     event_label: label,
     value: value,
-  })
-}
+  });
+};
 
-export const Gtag = () => <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+export const Gtag = () => (
+  <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+);
 
-export const PageView = () => <script
+export const PageView = () => (
+  <script
     dangerouslySetInnerHTML={{
       __html: `
     window.dataLayer = window.dataLayer || [];
@@ -30,3 +34,4 @@ export const PageView = () => <script
   `,
     }}
   />
+);
