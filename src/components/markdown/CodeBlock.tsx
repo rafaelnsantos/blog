f391943f102cd5fa27f79ce0adf6ed914f61af64
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-17 12:57:42 -0300

    lighter prism with PrismAsyncLight

--- src/components/markdown/CodeBlock.tsx
+++ src/components/markdown/CodeBlock.tsx
@@ -1,15 +1,15 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface CodeBlockProps {
-  value: string
-  language?: string
+  value: string;
+  language?: string;
 }
 
-export function CodeBlock (props: CodeBlockProps) {
+export function CodeBlock(props: CodeBlockProps) {
   return (
     <SyntaxHighlighter showLineNumbers language={props.language} style={atomDark}>
       {props.value}
     </SyntaxHighlighter>
-  )
+  );
 }
