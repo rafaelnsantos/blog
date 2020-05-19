6c1d83960f9b397938e30f7d7060a8bce63c4c1f
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-18 22:04:00 -0300

    fix toggle theme btn

--- src/components/header/ToggleThemeButton.tsx
+++ src/components/header/ToggleThemeButton.tsx
@@ -3,5 +3,7 @@ import useDarkMode from 'use-dark-mode';
 export function ToggleThemeButton() {
   const darkMode = useDarkMode(null, { storageKey: 'theme' });
 
+  if (darkMode.value === null) return null;
+
   return <button onClick={darkMode.toggle}>{darkMode.value ? 'light theme' : 'dark theme'}</button>;
 }
