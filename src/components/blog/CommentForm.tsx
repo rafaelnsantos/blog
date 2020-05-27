b9caec19c6135e90c40c76ce89a27dab21449ec5
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-27 16:53:41 -0300

    comment form

--- src/components/blog/CommentForm.tsx
+++ src/components/blog/CommentForm.tsx
@@ -1,5 +1,6 @@
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
 import { fetcher } from '~/services/fetcher';
 import { Comment } from '~/pages/blog/[slug]';
 
@@ -7,6 +8,8 @@ interface CommentFormProps {
   slug: string;
   onNewComment: (comment: Comment) => void;
 }
+
+const InputDiv = styled.div.attrs({ className: 'flex flex-col mb-2' })``;
 
 export function CommentForm({ slug, onNewComment }: CommentFormProps) {
   const formik = useFormik({
@@ -18,7 +21,7 @@ export function CommentForm({ slug, onNewComment }: CommentFormProps) {
       user: Yup.string().min(3, 'Too short!').required('Required!'),
       comment: Yup.string().required('Required!'),
     }),
-    onSubmit: async (values, { resetForm }) => {
+    onSubmit: async (values, { resetForm, setSubmitting }) => {
       const data = await fetcher(
         `
         mutation ($input: CommentInput!){
@@ -34,6 +37,7 @@ export function CommentForm({ slug, onNewComment }: CommentFormProps) {
         }
       );
       resetForm();
+      setSubmitting(false);
       onNewComment({
         id: data.newComment,
         comment: values.comment,
@@ -45,25 +49,34 @@ export function CommentForm({ slug, onNewComment }: CommentFormProps) {
 
   return (
     <form onSubmit={formik.handleSubmit} className="flex flex-col">
-      <label htmlFor="user">Name</label>
-      <input
-        id="user"
-        name="user"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.user}
-      />
-      {formik.errors.user}
-      <label htmlFor="comment">Comment</label>
-      <textarea
-        id="comment"
-        name="comment"
-        rows={4}
-        onChange={formik.handleChange}
-        value={formik.values.comment}
-      />
-      {formik.errors.comment}
-      <button type="submit">Submit</button>
+      <InputDiv>
+        <label htmlFor="user">Name</label>
+        <input
+          id="user"
+          name="user"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.user}
+        />
+        {formik.touched.user && formik.errors.user}
+      </InputDiv>
+      <InputDiv>
+        <label htmlFor="comment">Comment</label>
+        <textarea
+          id="comment"
+          name="comment"
+          rows={4}
+          onChange={formik.handleChange}
+          value={formik.values.comment}
+          className="p-4"
+        />
+        {formik.touched.comment && formik.errors.comment}
+      </InputDiv>
+      {!formik.isSubmitting && (
+        <button type="submit" disabled={formik.isSubmitting}>
+          Submit
+        </button>
+      )}
     </form>
   );
 }
