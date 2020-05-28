09123e1d44ec3ae76b883c06fc038a3d7d8a84a0
Rafael Santos <rafaelnsantos1@gmail.com>
2020-05-27 21:32:47 -0300

    font title markdown

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,12 @@ export default class MyDocument extends Document {
           <PageView />
           <link rel="preconnect" href="https://www.google-analytics.com" />
           <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com/" />
+          <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&family=Merriweather&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
