d1aab4a7751280a35d83007b6dbf8751fecdb31f
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-17 16:31:14 -0300

    add SEO to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
-import data from 'content/landingPage.json';
+import { NextSeo } from 'next-seo';
+import SEO from 'content/seo.json';
+
+const youtube = 'tgbNymZ7vqY';
 
 export default function Index() {
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <h2>{data.subtitle}</h2>
-    </div>
+    <NextSeo
+      title={SEO.title}
+      description={SEO.description}
+      openGraph={{
+        type: 'website',
+        locale: 'en_US',
+        url: process.env.NEXT_PUBLIC_URL,
+        images: [
+          {
+            url: SEO.image,
+            width: 1200,
+            height: 630,
+            alt: SEO.title,
+          },
+        ],
+      }}
+    >
+      <iframe
+        title="video"
+        width="420"
+        height="315"
+        src={`https://www.youtube.com/embed/${youtube}`}
+      />
+    </NextSeo>
   );
 }
