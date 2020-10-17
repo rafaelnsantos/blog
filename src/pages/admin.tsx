fb534326b68cba50e317cb5a953f74b8d2d60e4d
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-17 18:34:42 -0300

    move admin to pages + netlify cms locally

---
+++ src/pages/admin.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import { CmsConfig, CmsCollection } from 'netlify-cms-core';
+
+interface ConfigFixed extends CmsConfig {
+  local_backend?: boolean;
+  load_config_file?: boolean;
+}
+
+const collections: CmsCollection[] = [
+  {
+    name: 'blog',
+    label: 'Blog',
+    folder: 'blog',
+    create: true,
+    fields: [
+      { label: 'Title', name: 'title', widget: 'string' },
+      { label: 'Publish Date', name: 'date', widget: 'datetime' },
+      { label: 'Meta Title', name: 'metaTitle', widget: 'string' },
+      { label: 'Meta Description', name: 'metaDescription', widget: 'string' },
+      { label: 'Meta Image', name: 'metaImage', widget: 'image' },
+      { label: 'Body', name: 'body', widget: 'markdown' },
+      { label: 'Tags', name: 'tags', widget: 'list' },
+    ],
+  },
+  {
+    name: 'settings',
+    label: 'Settings',
+    extension: 'json',
+    editor: {
+      preview: false,
+    },
+    files: [
+      {
+        file: 'content/seo.json',
+        label: 'Search Engine Optimization',
+        name: 'seo',
+
+        fields: [
+          { name: 'title', label: 'Title', widget: 'string' },
+          { name: 'description', label: 'Description', widget: 'string' },
+          { name: 'image', label: 'Image', widget: 'image' },
+        ],
+      },
+    ],
+  },
+];
+
+const cmsConfig: ConfigFixed = {
+  backend: {
+    name: 'git-gateway',
+    branch: 'master',
+  },
+  media_folder: 'public/uploads',
+  public_folder: 'uploads',
+  collections,
+  local_backend: process.env.NODE_ENV !== 'production',
+  load_config_file: false,
+};
+
+const CMS = dynamic(
+  async () => {
+    const cms = (await import('netlify-cms-app')).default;
+    const Div = () => <div />;
+    cms.init({
+      config: cmsConfig,
+    });
+    return Div;
+  },
+  { ssr: false }
+);
+
+export default function AdminPage() {
+  return (
+    <div id="nc-root">
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+        />
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <CMS />
+    </div>
+  );
+}
