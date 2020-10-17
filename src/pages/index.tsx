ec1f68341a6b2e7af4721c8389247d9f489f6113
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-17 17:23:32 -0300

    fix page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,29 +5,30 @@ const youtube = 'tgbNymZ7vqY';
 
 export default function Index() {
   return (
-    <NextSeo
-      title={SEO.title}
-      description={SEO.description}
-      openGraph={{
-        type: 'website',
-        locale: 'en_US',
-        url: process.env.NEXT_PUBLIC_URL,
-        images: [
-          {
-            url: SEO.image,
-            width: 1200,
-            height: 630,
-            alt: SEO.title,
-          },
-        ],
-      }}
-    >
+    <>
+      <NextSeo
+        title={SEO.title}
+        description={SEO.description}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: process.env.NEXT_PUBLIC_URL,
+          images: [
+            {
+              url: SEO.image,
+              width: 1200,
+              height: 630,
+              alt: SEO.title,
+            },
+          ],
+        }}
+      />
       <iframe
         title="video"
         width="420"
         height="315"
         src={`https://www.youtube.com/embed/${youtube}`}
       />
-    </NextSeo>
+    </>
   );
 }
