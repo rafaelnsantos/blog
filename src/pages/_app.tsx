0ff88b73075df7a8efe75df8ecd1494c303ad60a
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-18 02:23:55 -0300

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import { AppPropsType } from 'next/dist/next-server/lib/utils';
-import { GlobalStyle } from '~/styles/global';
-
 import './app.scss';
 
 const MyApp = ({ Component, pageProps }: AppPropsType) => {
   return (
     <>
-      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
