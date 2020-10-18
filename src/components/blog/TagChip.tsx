2d8531fbf837294cfbc5747f5c2faf2340db9fc2
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-18 13:38:37 -0300

    tag style

--- src/components/blog/TagChip.tsx
+++ src/components/blog/TagChip.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
 interface TagChipProps {
   tag: string;
 }
 
+const Tag = styled.div`
+  background: var(--accent-green);
+  color: #050505;
+`;
+
 export function TagChip({ tag }: TagChipProps) {
-  return <div className="rounded mr-2 px-1 bg-green-500 text-xs">{tag}</div>;
+  return <Tag className="rounded mr-2 px-1 text-xs">{tag}</Tag>;
 }
