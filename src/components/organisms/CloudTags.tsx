7f4c25dbe162797bebf65915b40285155c25a796
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-21 05:21:24 -0300

    refactor CloudTag to organism

---
+++ src/components/organisms/CloudTags.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { TagCloud } from 'react-tagcloud';
+import styled from 'styled-components';
+import { useThemeMode } from '~/hooks/useThemeMode';
+import { CloudTag } from '~/utils/blogUtils';
+
+const StyledTagClod = styled(TagCloud)`
+  cursor: pointer;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+interface CloudTagsProps {
+  onSelectTag: (tag: string | null) => void;
+  tags: CloudTag[];
+}
+
+export function CloudTags(props: CloudTagsProps) {
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [luminosity, setLuminosity] = useState('');
+
+  useThemeMode((darkMode) => setLuminosity(darkMode.value ? 'light' : 'dark'));
+
+  useEffect(() => {
+    props.onSelectTag(selectedTag);
+  }, [selectedTag]);
+
+  return (
+    <div className="flex justify-center md:max-w-screen-md m-auto px-10">
+      {selectedTag ? (
+        <>
+          <div>{selectedTag}</div>
+          <button onClick={() => setSelectedTag(null)}>clear tag</button>
+        </>
+      ) : (
+        <StyledTagClod
+          colorOptions={{ luminosity }}
+          minSize={12}
+          maxSize={24}
+          tags={props.tags}
+          onClick={(tag: CloudTag) => setSelectedTag(tag.value)}
+        />
+      )}
+    </div>
+  );
+}
--- src/components/templates/Blog.tsx
+++ src/components/templates/Blog.tsx
@@ -1,51 +1,19 @@
 import { useState } from 'react';
-import { TagCloud } from 'react-tagcloud';
-import styled from 'styled-components';
-import useDarkMode from 'use-dark-mode';
 import { BlogProps } from '~/pages/blog/index';
-import { CloudTag } from '~/utils/blogUtils';
+import { CloudTags } from '../organisms/CloudTags';
 import { PostList } from '../organisms/PostList';
 
-const StyledTagClod = styled(TagCloud)`
-  cursor: pointer;
-
-  display: flex;
-
-  flex-wrap: wrap;
-
-  justify-content: center;
-`;
-
 export function BlogTemplate({ posts, tags, preview }: BlogProps) {
-  const [selectedTag, setSelectedTag] = useState<string | null>(null);
-  const darkMode = useDarkMode(undefined, { storageKey: 'dark' });
-
-  const postsToShow = selectedTag
-    ? posts.filter((post) => post.tags.includes(selectedTag as string))
-    : posts;
-
-  const clearTag = () => setSelectedTag(null);
-
-  const selectTag = (tag: CloudTag) => setSelectedTag(tag.value);
+  const [postsToShow, setPostsToShow] = useState(posts);
 
   return (
     <div className="md:max-w-screen-xl m-auto">
-      <div className="flex justify-center md:max-w-screen-md m-auto px-10">
-        {selectedTag ? (
-          <>
-            <div>{selectedTag}</div>
-            <button onClick={clearTag}>clear tag</button>
-          </>
-        ) : (
-          <StyledTagClod
-            colorOptions={{ luminosity: darkMode.value ? 'light' : 'dark' }}
-            minSize={12}
-            maxSize={24}
-            tags={tags}
-            onClick={selectTag}
-          />
-        )}
-      </div>
+      <CloudTags
+        onSelectTag={(tag) =>
+          setPostsToShow(tag ? posts.filter((post) => post.tags.includes(tag)) : posts)
+        }
+        tags={tags}
+      />
       <PostList posts={postsToShow} preview={preview} />
     </div>
   );
