708a8363bf0ce7716d8b1e4ce90315b14d4d840d
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-21 06:31:12 -0300

    refactor + use reduce

--- src/components/admin/previews/ColorPreview.tsx
+++ src/components/admin/previews/ColorPreview.tsx
@@ -2,49 +2,52 @@ import { PreviewTemplateComponentProps } from 'netlify-cms-core';
 import { Post as PostType, PostPreview } from '~/utils/blogUtils';
 import { Preview } from './Preview';
 import COLORS from 'content/colors.json';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { HeaderNavPreview } from '~/components/header/HeaderNav';
 import { LandingPageTemplate } from '~/components/templates/LandingPage';
 import { AboutPageTemplate } from '~/components/templates/AboutPage';
 import { PostTemplate } from '~/components/templates/Post';
 import { BlogTemplate } from '~/components/templates/Blog';
 
+const post: PostType = {
+  title: 'asd',
+  content: `# Hello`,
+  timestamp: new Date().getTime(),
+  meta: {
+    description: 'asdasd',
+    image: 'MonxClosed.png',
+    title: 'asdasd',
+  },
+  readingTime: 1,
+  slug: 'slug',
+  star: true,
+  tags: ['asdasd'],
+};
+
+const postPreview: PostPreview = {
+  meta: post.meta,
+  slug: post.slug,
+  tags: post.tags,
+  timestamp: post.timestamp,
+  title: post.title,
+};
+
 export function ColorPreview({ entry }: PreviewTemplateComponentProps) {
   const get = useCallback((keys: string[]) => entry.getIn(['data', ...keys]), [entry]);
 
   const [selectedTemplate, setSelectedTemplate] = useState(<LandingPageTemplate />);
 
-  const colors: any = {};
-
-  Object.keys(COLORS).map((color) => {
-    colors[color] = {
-      light: get([color, 'light']),
-      dark: get([color, 'dark']),
-    };
-  });
-
-  const post: PostType = {
-    title: 'asd',
-    content: '',
-    timestamp: new Date().getTime(),
-    meta: {
-      description: 'asdasd',
-      image: 'MonxClosed.png',
-      title: 'asdasd',
-    },
-    readingTime: 1,
-    slug: 'slug',
-    star: true,
-    tags: ['asdasd'],
-  };
-
-  const postPreview: PostPreview = {
-    meta: post.meta,
-    slug: post.slug,
-    tags: post.tags,
-    timestamp: post.timestamp,
-    title: post.title,
-  };
+  const colors = useMemo(
+    () =>
+      Object.keys(COLORS).reduce((prev, color) => {
+        prev[color as keyof typeof COLORS] = {
+          light: get([color, 'light']),
+          dark: get([color, 'dark']),
+        };
+        return prev;
+      }, {} as typeof COLORS),
+    [entry]
+  );
 
   return (
     <Preview colors={colors}>
