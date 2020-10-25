6c37d30941691d63c7a0808adcc2f5bbedf910d3
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-25 02:11:08 -0300

    provider

--- src/providers/GoogleAnalytics.tsx
+++ src/providers/GoogleAnalytics.tsx
@@ -1,16 +1,17 @@
 import ReactGA from 'react-ga';
-import { ProviderBase } from './Provider';
+import { GenericProvider } from './Provider';
 
 interface AnalitycsProviderProps {
   trackingId: string;
 }
 
-export const [GoogleAnalyticsProvider, useGoogleAnalytics] = ProviderBase<
-  typeof ReactGA,
-  AnalitycsProviderProps
+export const [GoogleAnalyticsProvider, useGoogleAnalytics] = GenericProvider<
+  AnalitycsProviderProps,
+  typeof ReactGA
 >(({ trackingId }) => {
   ReactGA.initialize(trackingId, {
     debug: process.env.NODE_ENV !== 'production',
   });
+
   return ReactGA;
 }, ReactGA);
--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -1,16 +1,16 @@
 import React, { createContext, useContext, useMemo } from 'react';
 
-export function ProviderBase<T, Params>(
+export function GenericProvider<Params, T>(
   initializer: (params: Params) => T,
   initial: T
-): [React.FC<Params>, () => T] {
-  const analitycsContext = createContext<T>(initial);
+): [React.FC<Params>, () => typeof initial] {
+  const genericContext = createContext<T>(initial);
 
   const Provider: React.FC<Params> = (props) => {
     const value = useMemo(() => initializer(props), []);
-    return <analitycsContext.Provider value={value}>{props.children}</analitycsContext.Provider>;
+    return <genericContext.Provider value={value}>{props.children}</genericContext.Provider>;
   };
 
-  const hook = () => useContext(analitycsContext);
+  const hook = () => useContext(genericContext);
   return [Provider, hook];
 }
