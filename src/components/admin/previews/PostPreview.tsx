32e06ad59d756411798736ae2c3b1227b732d6a1
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-31 22:22:53 -0300

    post preview page

--- src/components/admin/previews/PostPreview.tsx
+++ src/components/admin/previews/PostPreview.tsx
@@ -2,16 +2,19 @@ import { PostTemplate } from '~/components/templates/Post';
 import { Post } from '~/utils/blogUtils';
 import { getReadingTime } from '~/utils/getReadingTime';
 import { Preview } from '@monx/react-netlifycms/dist/Preview';
+import { PreviewPage } from './PreviewPage';
 
 export const PostPreview = Preview<Post>(({ values }) => {
   return (
-    <PostTemplate
-      post={{
-        ...values,
-        authors: Object.values(values.authors),
-        tags: Object.values(values.tags),
-        readingTime: getReadingTime(values.body),
-      }}
-    />
+    <PreviewPage>
+      <PostTemplate
+        post={{
+          ...values,
+          authors: Object.values(values.authors),
+          tags: Object.values(values.tags),
+          readingTime: getReadingTime(values.body),
+        }}
+      />
+    </PreviewPage>
   );
 });
